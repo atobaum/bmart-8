8d71e16114b097bd131e6613302ace0f9d05abee
nohgijin <ynda11111@naver.com>
2020-08-13 21:10:33 +0900

    feat: logo component 작성 스크롤 해도 고정 되도록 제작

---
+++ client/src/components/Logo.js
@@ -0,0 +1,22 @@
+import React, { useState, Fragment } from 'react';
+import styled, { css } from 'styled-components';
+import LogoIcon from '../static/icon/icon-logo.png';
+
+const LogoBlock = styled.div`
+  position: fixed;
+  width: 100vw;
+  margin: 0 auto;
+  img {
+    height: 40px;
+  }
+`;
+
+const Logo = () => {
+  return (
+    <LogoBlock>
+      <img src={LogoIcon} />
+    </LogoBlock>
+  );
+};
+
+export default Logo;
