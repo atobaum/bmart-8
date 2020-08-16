1097d294b1d6db9f4aca8ab7e9ee3703c818c4d2
Atobaum <atobaum@gmail.com>
2020-08-16 22:20:54 +0900

    feat: Add carousel to main page

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from '../components/Header';
-const MainPageBlock = styled.div``;
+import Carousel from '../components/Carousel';
+const MainPageBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const MainPage: React.FC = () => {
   return (
     <MainPageBlock>
       <Header></Header>
+      <Carousel></Carousel>
     </MainPageBlock>
   );
 };
