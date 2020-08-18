49b1ecad5645e3494fd7842ed30d49106d5db982
Atobaum <atobaum@gmail.com>
2020-08-18 11:17:18 +0900

    feat: Add banner selector
    - Change banner when selector clicked

--- client/src/components/Carousel.tsx
+++ client/src/components/Carousel.tsx
@@ -46,18 +46,41 @@ type CarouselProps = {
     routeUrl: string;
   }[];
 };
+
+const Selector: React.FC<{
+  length: number;
+  onChange: (idx: number) => void;
+}> = ({ length, onChange }) => {
+  return (
+    <div>
+      {Array(length)
+        .fill(0)
+        .map((_, idx) => {
+          return <button onClick={() => onChange(idx)}>idx</button>;
+        })}
+    </div>
+  );
+};
+
 const Carousel: React.FC<CarouselProps> = ({ images }) => {
   // to get width of the container
   const containerRef = useRef(null);
   const [curBanner, setCurBanner] = useState(0);
+  const [timeoutId, setTimeoutId] = useState<number | null>(null);
   useEffect(() => {
     const container = containerRef.current as any;
     container.scroll(curBanner * container.offsetWidth, 0);
 
-    setTimeout(
+    if (timeoutId !== null) {
+      clearTimeout(timeoutId);
+      setTimeoutId(null);
+    }
+
+    const newTimeoutId = setTimeout(
       () => setCurBanner(curBanner < images.length - 1 ? curBanner + 1 : 0),
       3000
     );
+    setTimeoutId(newTimeoutId);
   }, [curBanner]);
 
   return (
@@ -73,6 +96,7 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
           );
         })}
       </div>
+      <Selector length={images.length} onChange={(idx) => setCurBanner(idx)} />
     </CarouselBlock>
   );
 };
