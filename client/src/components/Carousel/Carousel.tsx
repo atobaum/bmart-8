44f469ba5d86d0314947d5573258dd79210ce688
Atobaum <atobaum@gmail.com>
2020-08-19 13:37:52 +0900

    refactor: CarouselSelector 파일로 분리

--- client/src/components/Carousel/Carousel.tsx
+++ client/src/components/Carousel/Carousel.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import CarouselSelector from './CarouselSelector';
 
 const CarouselBlock = styled.div<{
   isDragging: boolean;
@@ -50,54 +51,6 @@ type CarouselProps = {
     routeUrl: string;
   }[];
   transitionTime: number;
-};
-
-const SelectorBlock = styled.div`
-  position: relative;
-  margin-top: -30px;
-  button {
-    margin: 0.3rem;
-    padding: 0;
-    border: none;
-    background: none;
-
-    .selector {
-      background: #d3d3d3;
-      width: 0.8rem;
-      height: 0.8rem;
-      border-radius: 50%;
-
-      &.active {
-        background: white;
-      }
-    }
-    .selector::before {
-      content: '';
-    }
-  }
-`;
-
-const Selector: React.FC<{
-  length: number;
-  onChange: (idx: number) => void;
-  activeIdx: number;
-}> = ({ length, onChange, activeIdx }) => {
-  return (
-    <SelectorBlock>
-      {Array(length)
-        .fill(0)
-        .map((_, idx) => {
-          return (
-            <button key={idx} onClick={() => onChange(idx + 1)}>
-              <div
-                className={`selector${
-                  activeIdx === idx ? ' active' : ''
-                }`}></div>
-            </button>
-          );
-        })}
-    </SelectorBlock>
-  );
 };
 
 const Carousel: React.FC<CarouselProps> = ({ images, transitionTime }) => {
@@ -196,7 +149,7 @@ const Carousel: React.FC<CarouselProps> = ({ images, transitionTime }) => {
           </Link>
         </div>
       </div>
-      <Selector
+      <CarouselSelector
         length={images.length}
         onChange={(idx) => setCurBanner(idx)}
         activeIdx={(curBanner - 1 + images.length) % images.length}
