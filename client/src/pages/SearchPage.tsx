26e154a7a711cd76584c86b1f8fe44e2ef90f740
Atobaum <atobaum@gmail.com>
2020-08-20 13:30:55 +0900

    feat: Load search history and render in search page

--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -1,13 +1,53 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { useQuery } from 'react-apollo';
+import { gql } from 'apollo-boost';
+import { formatDistanceToNow } from 'date-fns';
+import { ko } from 'date-fns/locale';
+
+const SearchHistoryItem: React.FC<{
+  onDelete: () => void;
+  date: Date;
+  query: string;
+}> = ({ onDelete, date, query }) => {
+  return (
+    <div>
+      <div>{query}</div>
+      <div>{formatDistanceToNow(date, { locale: ko })} 전</div>
+      <button onClick={onDelete}>X</button>
+    </div>
+  );
+};
 
 const SearchPageBlock = styled.div``;
 
 const SearchPage: React.FC = () => {
   const [query, setQuery] = useState('');
+  const { data, loading, error } = useQuery(gql`
+    query getSearchHistory {
+      searchHistory {
+        id
+        date
+        query
+      }
+    }
+  `);
+
   return (
     <SearchPageBlock>
       <input value={query} onChange={(e) => setQuery(e.target.value)} />
+      {loading || error
+        ? null
+        : data.searchHistory.map(({ id, date, query }: any) => {
+            return (
+              <SearchHistoryItem
+                key={id}
+                onDelete={() => console.log('search histroy delete', id)}
+                date={new Date(date)}
+                query={query}
+              />
+            );
+          })}
     </SearchPageBlock>
   );
 };
