193ac04fc55f3e3f95155b2064c65d7ac72b7ece
Atobaum <atobaum@gmail.com>
2020-08-21 11:18:43 +0900

    fix: Change dummy image

--- server/src/graphql/resolvers/banner.ts
+++ server/src/graphql/resolvers/banner.ts
@@ -10,7 +10,12 @@ type Banner = {
 
 export default {
   Query: {
-    mainBanners: (_: any, { type }: any): Banner[] => {
+    mainBanners: async (): Promise<Banner[]> => {
+      //     const data = await prisma.banner.count();
+      //   const data = await prisma.category_first.findMany({
+      //     include: { children: true },
+      //   });
+      //   const data = await prisma.banner.findMany();
       return [
         {
           type: 'main',
@@ -24,6 +29,13 @@ export default {
           altString: '책도 배달 가능',
           imgUrl:
             'https://bmart-8.s3.ap-northeast-2.amazonaws.com/assets/banners/0b776996-d151-447f-8f6a-0bb3ebdbf568.gif',
+          routeUrl: '/',
+        },
+        {
+          type: 'main',
+          altString: '추억의 과자',
+          imgUrl:
+            'https://bmart-8.s3.ap-northeast-2.amazonaws.com/assets/banners/1c5c8ab7-913a-422c-bee3-9a47220abdc9.gif',
           routeUrl: '/',
         },
       ];
