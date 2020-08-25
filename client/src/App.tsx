1088a36c11ff2720dbbd0d06e659556a70174144
nohgijin <ynda11111@naver.com>
2020-08-25 10:10:12 +0900

    feat: Route 설정 url 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,6 +14,7 @@ import Footer from './components/Footer';
 import CategoryPage from './pages/CategoryPage';
 import SearchPage from './pages/SearchPage';
 import SearchResultPage from './pages/SearchResultPage';
+import CategoryDetailPage from './pages/CategoryDetailPage';
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -43,6 +44,7 @@ function App() {
             <Route path="/search">
               <SearchPage />
             </Route>
+            <Route path="/category/:query" component={CategoryDetailPage} />
             <Route path="/">
               <MainPage />
             </Route>
