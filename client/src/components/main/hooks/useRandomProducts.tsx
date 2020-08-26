0cf0ad34d1635fb9f7a4d915b6de160479b19b71
Atobaum <atobaum@gmail.com>
2020-08-26 16:06:47 +0900

    chore: useRandomProducts 타입 지정

--- client/src/components/main/hooks/useRandomProducts.tsx
+++ client/src/components/main/hooks/useRandomProducts.tsx
@@ -1,12 +1,19 @@
-import { GET_PRODUCT_SIMPLE } from '../main-query';
-import getRandomInt from '../../../utils/random';
-import { useQuery } from 'react-apollo';
 import { useState } from 'react';
+import { useQuery } from 'react-apollo';
+import getRandomInt from '../../../utils/random';
+import { GET_PRODUCT_SIMPLE } from '../main-query';
 
+type ProductInfo = {
+  id: number;
+  name: string;
+  price: number;
+  img_url: string;
+  discount: number;
+};
 // For dev
-export default function useRandomProducts(take: number) {
+export default function useRandomProducts(take: number): ProductInfo[] {
   const [cursor] = useState(getRandomInt(0, 1000));
-  const { data, loading, error } = useQuery(GET_PRODUCT_SIMPLE, {
+  const { data } = useQuery(GET_PRODUCT_SIMPLE, {
     variables: { cursor, take },
   });
 
